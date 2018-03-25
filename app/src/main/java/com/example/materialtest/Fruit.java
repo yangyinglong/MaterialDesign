4b388079dd8e074c782cc530bf7650f2d0e2ae46
yangyinglong <15968161237@163.com>
2018-03-26 02:01:29 +0800

    this is first commit, it is Material Design

---
+++ app/src/main/java/com/example/materialtest/Fruit.java
@@ -0,0 +1,25 @@
+package com.example.materialtest;
+
+/**
+ * Created by 她是龙 on 2018/3/25.
+ */
+
+public class Fruit {
+
+    private String name;
+
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
